66aba0179509bc4e7f3b89b46dea0cbcd51c5ce7
Виктор Тяжельников <vtyazhelnikov@swaxis.com>
2018-07-18 21:51:36 +0600

    Initial commit

---
+++ index.ts
@@ -0,0 +1,61 @@
+import * as path from "path";
+import * as norman from "node-norman";
+
+const convertSourceMap = require("convert-source-map");
+
+
+export default class CoffeeScriptPlugin extends norman.Plugin {
+  matches(filename: string, module: norman.ModuleInfo): boolean {
+    return filename.endsWith(".coffee");
+  }
+
+  async transform(fileContent: string, sourceFilePath: string, targetFilePath: string, module: norman.ModuleInfo): Promise<norman.TransformedFile[]> {
+    const cs = require("coffee-script");
+
+    let compiled = cs.compile(fileContent, {
+      inlineMap: true
+    });
+
+    // now extract source map and make it external
+    let smRegexp = /(\/\/#\s+sourceMappingURL=[\s\S]+)\/\/#\s+sourceURL=coffeescript/;
+    let match = smRegexp.exec(compiled);
+    if (match) {
+      let sm = match[1];
+      let extractedSourceMap = convertSourceMap.fromComment(sm)
+          .setProperty("sourceRoot", path.dirname(sourceFilePath))
+          .setProperty("sources", [ path.basename(sourceFilePath) ])
+          .setProperty("sourcesContent", undefined)
+          .setProperty("file", path.basename(targetFilePath))
+          .toJSON();
+
+      let sourceMapFile = path.basename(targetFilePath).replace(/\.coffee$/, ".js.map");
+
+      compiled = compiled.replace(smRegexp, "") + `//# sourceMappingURL=${sourceMapFile}`;
+
+      return [
+        {
+          content: compiled,
+          filename: targetFilePath.replace(/\.coffee$/, ".js")
+        },
+        {
+          content: extractedSourceMap,
+          filename: path.join(path.dirname(targetFilePath), sourceMapFile)
+        }
+      ];
+    }
+
+    return [
+      {
+        content: fileContent,
+        filename: targetFilePath
+      }
+    ];
+  }
+
+  clean(sourceFilePath: string, targetFilePath: string) {
+    return [
+      targetFilePath,
+      targetFilePath.replace(/\.js$/, ".js.map")
+    ];
+  }
+}
